431632f986db8bf9459627605b8ff93e71ba5c91
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-05 12:34:08 +0545

    created a new server file with typescript

---
+++ backend-express/server.ts
@@ -0,0 +1,24 @@
+import { configDotenv } from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+import { connectDB } from './src/configs/database'
+configDotenv()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+const startServer = async () => {
+    try {
+        await connectDB()
+        app.listen(port, () => console.log(`server is running at ${port}`))
+    } catch (error) {
+
+    }
+}
+startServer()
+

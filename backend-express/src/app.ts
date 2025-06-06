fdc0b2b886e160944200eb75c1ec85f394482d35
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-06 22:52:09 +0545

    import errorhandler middleware and neccessary import for the sever

--- backend-express/src/app.ts
+++ backend-express/src/app.ts
@@ -1,7 +1,17 @@
 import express, { Request, Response } from 'express'
 import routes from './routes/index'
+import errorHandler from './middlewares/error-handler'
+import morgan from 'morgan'
 const app = express()
 
+
+//middleware
+app.use(morgan('dev'))
+app.use(express.json())
+
 app.use('/api', routes)
+
+//error handling middleware
+app.use(errorHandler as any)
 
 export default app

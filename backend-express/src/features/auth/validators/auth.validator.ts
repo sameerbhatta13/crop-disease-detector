badf3e43b0107beb95817b24d106f8646152aa9c
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-08 17:31:31 +0545

    created a auth validatoir files for validation

---
+++ backend-express/src/features/auth/validators/auth.validator.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { USER_ROLES } from "../../../constants/constants";
+
+
+export const loginValidator = z.object({
+    email: z.string().email('Invalid email format'),
+    password: z.string().min(1, 'password is required')
+})
+
+export const signupValidator = z.object({
+    name: z.string().min(2, { message: 'name is atleat 2 character long' }).max(50, { message: 'name is at most 50 character long' }).regex(/[^a-zA-Z0-9]/, { message: 'name can only contains letters and spaces' }),
+
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z
+        .string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+        .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+        .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+        .regex(/[^a-zA-Z0-9]/, { message: 'Password must contain at least one special character' }),
+
+    user: z.enum(Object.values(USER_ROLES) as any).refine(value => Object.values(USER_ROLES).includes(value as any), { message: 'invalid role' })
+})
+
+
+export const emailValidator = z.object({
+    email: loginValidator.shape.email
+})
+
+
+export const refreshTokenValidator = z.object({
+    refreshToken: z.string().min(1, 'refresh token is required')
+})

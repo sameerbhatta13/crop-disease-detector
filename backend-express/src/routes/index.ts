e94913160b3cdbf452f7cd512cc77b9a27fe73e4
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-08 17:31:45 +0545

    import authroutes

--- backend-express/src/routes/index.ts
+++ backend-express/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import profileRoutes from '../features/profile/route/profile.routes'
+import authRoutes from '../features/auth/routes/auth.routes'
 
 const router = express.Router()
 
 
+router.use('/auth', authRoutes)
 router.use('/profile', profileRoutes)
 
 export default router

5fd0b5899089be0dc444f59dde6a1a6c47397c0b
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-09 11:11:31 +0545

    added a verify-email routes

--- backend-express/src/features/auth/routes/auth.routes.ts
+++ backend-express/src/features/auth/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register } from "../controller/auth.controller";
+import { login, register, verifyEmail } from "../controller/auth.controller";
 import { validator } from "../../../middlewares/validator";
 import { loginValidator, signupValidator } from "../validators/auth.validator";
 
@@ -8,6 +8,7 @@ const router = express.Router()
 
 router.post('/register', validator(signupValidator), register)
 router.post('/login', validator(loginValidator), login)
+router.get('/verify-email/:token', verifyEmail)
 
 
 export default router

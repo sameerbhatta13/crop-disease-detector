f3ed767509f01a5b93080924f152fffa0e1b5f6d
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-09 17:35:11 +0545

    create a home folder

---
+++ mobile-app/app/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+const index = () => {
+    return (
+        <>
+            <View className='mx-10 my-10 '>
+                <Text>HELLO FROM HOME PAGE</Text>
+            </View>
+        </>
+    )
+}
+
+export default index

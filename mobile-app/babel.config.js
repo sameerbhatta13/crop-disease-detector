ff6e52647e0756778f097f484b4061c927581b7c
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-10 22:11:39 +0545

    import necessary package

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -6,6 +6,7 @@ module.exports = function (api) {
             "nativewind/babel",
         ],
         plugins: [
+            "expo-router/babel",
             'react-native-reanimated/plugin'
         ]
     };

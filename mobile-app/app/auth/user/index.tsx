99e1111171adac5fcd8689e74f30b36747954f75
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-10 22:12:45 +0545

    setup folder strucuture

---
+++ mobile-app/app/auth/user/index.tsx
@@ -0,0 +1,13 @@
+// rnfe for import this boiler plate
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+
+export default index

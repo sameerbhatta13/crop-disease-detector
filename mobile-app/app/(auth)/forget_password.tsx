d78433094d2cb869a66b2f4ad02e2a4d5eca3c05
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-10 22:13:04 +0545

    folder setup structure

---
+++ mobile-app/app/(auth)/forget_password.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const forget_password = () => {
+    return (
+        <View>
+            <Text>forget-password</Text>
+        </View>
+    )
+}
+
+export default forget_password
+
+const styles = StyleSheet.create({})

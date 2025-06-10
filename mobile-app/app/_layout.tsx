3bd921a6f27ede8e3c79a1abb81821767c6b8d79
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-10 22:13:31 +0545

    initial setup

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,6 +1,17 @@
-// app/_layout.tsx
-import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack initialRouteName="index" screenOptions={{ headerShown: false }} />;
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='(tabs)' options={{
+        headerShown: false
+      }} />
+    </Stack>
+  )
 }
+
+export default RootLayout
+
+// const styles = StyleSheet.create({})
--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -22,3 +22,4 @@ const index = () => {
 }
 
 export default index
+

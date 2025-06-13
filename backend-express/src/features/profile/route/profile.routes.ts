240098d442475fa0bc8d052052823d8563381fba
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-13 17:49:03 +0545

    create a routes for create user profile

--- backend-express/src/features/profile/route/profile.routes.ts
+++ backend-express/src/features/profile/route/profile.routes.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import { authenticateUser } from '../../../middlewares/auth'
+import { upload } from '../../../configs/multer.config'
+import { createProfileSchema } from '../validators/profile.validator'
+import { validator } from '../../../middlewares/validator'
+import { createProfile } from '../controller/profile.controller'
 
 
 const router = express.Router()
 
-
-router.post('/', authenticateUser)
+router.post('/', authenticateUser, upload.fields([{ name: 'avatar', maxCount: 1 }]), validator(createProfileSchema, "body"), createProfile)
 
 
 export default router

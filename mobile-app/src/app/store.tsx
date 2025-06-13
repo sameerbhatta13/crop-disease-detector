0d06db5bd6a1b9b7827328975ceaf96faeb8e483
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-13 10:24:49 +0545

    create a store for api redux

---
+++ mobile-app/src/app/store.tsx
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { AuthApi } from '../redux/AuthApi'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+export const store = configureStore({
+    reducer: {
+        [AuthApi.reducerPath]: AuthApi.reducer
+
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(AuthApi.middleware)
+})
+
+setupListeners(store.dispatch)

7439f2aa33c85932f5279589842fbe929dfef1ec
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-13 10:21:07 +0545

    add provide to the root layout

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,14 +1,22 @@
 
 import React from 'react'
 import { Stack } from 'expo-router'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Provider } from 'react-redux'
+import { store } from '@/src/app/store'
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{
-        headerShown: false
-      }} />
-    </Stack>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name='index' options={{ headerShown: false }} />
+          <Stack.Screen name='(auth)' options={{
+            headerShown: false
+          }} />
+        </Stack>
+      </SafeAreaProvider>
+    </Provider>
   )
 }
 

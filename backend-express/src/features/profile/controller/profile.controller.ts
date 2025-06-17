0450fb43331313f7632a1b1c0b7c855b382b2663
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-17 12:20:19 +0545

    create a update profile controller

--- backend-express/src/features/profile/controller/profile.controller.ts
+++ backend-express/src/features/profile/controller/profile.controller.ts
@@ -3,6 +3,7 @@ import { AuthRequest } from '../../../middlewares/auth'
 import { Response } from 'express'
 import profileService from '../service/profile.service'
 import { sendBadRequest, sendSuccess } from '../../../utils/responseUtils'
+import { updateAvatar } from '../../../configs/multer.config'
 
 export const createProfile = asyncHandler(async (req: AuthRequest, res: Response) => {
     const profileData = { ...req.body, user: req.userId }
@@ -26,5 +27,24 @@ export const getProfile = asyncHandler(async (req: AuthRequest, res: Response) =
     }
 
     return sendSuccess(res, 'profile is retrieved successfully', result.data)
+
+})
+
+export const updateProfile = asyncHandler(async (req: AuthRequest, res: Response) => {
+    const userId = req.userId
+    const files = req.files as any
+
+    if (!userId) return sendBadRequest(res, 'userId is not provided')
+    const profile = await profileService.getProfile(req.userId as string)
+    if (files?.avatar?.[0]) {
+        const avatarPath = await updateAvatar(profile?.data?.avatarUrl, files?.avatar?.[0])
+        req.body.avatarUrl = avatarPath
+    }
+
+    const result = await profileService.updateProfile(userId, req.body)
+    if (!result.success) {
+        return sendBadRequest(res, result.message)
+    }
+    return sendSuccess(res, "profile is updated successfully", result.data)
 
 })

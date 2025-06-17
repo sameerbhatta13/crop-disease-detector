95b6f2c2496aff2840a6689b21d144fd2e65d14e
sameer bhatta <sameerbhatta37@gmail.com>
2025-06-17 12:23:54 +0545

    creates a route for update and get profile

--- backend-express/src/features/profile/route/profile.routes.ts
+++ backend-express/src/features/profile/route/profile.routes.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import { authenticateUser } from '../../../middlewares/auth'
 import { upload } from '../../../configs/multer.config'
-import { createProfileSchema } from '../validators/profile.validator'
+import { createProfileSchema, updateProfileSchema } from '../validators/profile.validator'
 import { validator } from '../../../middlewares/validator'
-import { createProfile } from '../controller/profile.controller'
+import { createProfile, getProfile, updateProfile } from '../controller/profile.controller'
 
 
 const router = express.Router()
 
 router.post('/', authenticateUser, upload.fields([{ name: 'avatar', maxCount: 1 }]), validator(createProfileSchema, "body"), createProfile)
+router.get('/', authenticateUser, getProfile)
+router.patch('/', authenticateUser, upload.fields([{ name: 'avatar', maxCount: 1 }]), validator(updateProfileSchema), updateProfile)
+
 
 
 export default router
